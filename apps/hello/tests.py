c869ecf78bd7d0d27394cb3c959a31d58e21d4fc
Valentyn <vcherkasov321@gmail.com>
2020-04-14 14:23:57 +0300

    ticket1: Added test for view.

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,20 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
 
 class SomeTests(TestCase):
     def test_math(self):
         "put docstrings in your tests"
         assert(2 + 2 == 4)
+
+
+class TestViews(TestCase):
+    def test_contacts_view(self):
+        """compare"""
+        client = Client()
+        response = client.get(reverse('main'))
+        self.assertTemplateUsed(response, 'hello/main.html')
+        self.assertContains(response, "Valentyn")
+        self.assertContains(response, "Cherkasov")
+        self.assertContains(response, "vcherkasov321@gmail.com")
+        self.assertContains(response, "valentyn17@42cc.co")
