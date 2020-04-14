dbd40c5bd0f58bfa4ca18c3902689367f093811b
Valentyn <vcherkasov321@gmail.com>
2020-04-14 14:36:18 +0300

    Added test for the model.

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
+from apps.hello.models import Developer
 
 
 class SomeTests(TestCase):
@@ -18,3 +19,17 @@ class TestViews(TestCase):
         self.assertContains(response, "Cherkasov")
         self.assertContains(response, "vcherkasov321@gmail.com")
         self.assertContains(response, "valentyn17@42cc.co")
+
+
+class TestModels(TestCase):
+    def test_required_fileds(self):
+        """Checks, if all required field are present"""
+        self.dev1 = Developer.objects.get(id=1)
+        fields = Developer._meta.fields
+        required_f = []
+        required_f_values = []
+        for field in fields:  # checks all required fileds
+            if not field.blank:
+                required_f.append(str(field))
+        for field in required_f_values:
+            assert(field in self.dev1._meta.fields)
